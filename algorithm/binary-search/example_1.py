5a94b8219b0f39ec1a135e6112c16b7779a53558
杨球松 <yangqiusong@cloudwalk.cn>
2021-03-17 17:39:53 +0800

    update binary search

--- algorithm/binary-search/example_1.py
+++ algorithm/binary-search/example_1.py
@@ -7,6 +7,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def binary_search_left(arr: list, target):
+    """变种1: 查找数组中第一个等于目标值的索引号"""
+    pass 
+
+
 if __name__ == '__main__':
     logging.basicConfig(format="[%(asctime)s %(filename)s:%(lineno)s] %(message)s",
                         level=logging.INFO,
