c8056d8141f8314285f48cc7d2397ef70c79f362
QiuSongYang <15962230327@163.com>
2021-03-17 21:13:47 +0800

    "update binary search"

---
+++ algorithm/binary-search/example_2.py
@@ -0,0 +1,14 @@
+"""
+# 二分查找变种
+"""
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format="[%(asctime)s %(filename)s:%(lineno)s] %(message)s",
+                        level=logging.INFO,
+                        filemode="a",
+                        filename=None)
