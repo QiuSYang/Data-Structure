de7eb2b6700adef538cd3dca0a282c642ba82628
QiuSongYang <15962230327@163.com>
2021-03-26 21:05:29 +0800

    "update binary tree"

---
+++ base-data-structure/red-black-tree/red_black_tree.py
@@ -0,0 +1,15 @@
+"""
+# 红黑树
+"""
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format="[%(asctime)s %(filename)s: %(lineno)s] %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        level=logging.INFO,
+                        filename=None,
+                        filemode="a")
