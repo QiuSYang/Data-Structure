7bb9cce9ca2c9cc0f9920c5b4d127e8d3f389987
QiuSongYang <15962230327@163.com>
2021-04-08 22:39:32 +0800

    "update graph"

---
+++ algorithm/greedy/huffman_encoding.py
@@ -0,0 +1,14 @@
+"""
+# 霍夫曼编码
+"""
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format="[%(asctime)s %(filename)s:%(lineno)s] %(message)s",
+                        level=logging.INFO,
+                        filemode="a",
+                        filename=None)
