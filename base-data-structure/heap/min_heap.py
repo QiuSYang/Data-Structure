19594f86c58e3a068042742437c39406ef209659
QiuSongYang <15962230327@163.com>
2021-04-17 21:23:21 +0800

    "update heap"

--- base-data-structure/heap/min_heap.py
+++ base-data-structure/heap/min_heap.py
@@ -91,9 +91,79 @@ class MinHeap(object):
         # 此时只有堆尾的节点是未排序的, 将添加的节点迭代到正确的位置
         self._siftdown(0, n)
 
+    @property
+    def heap_datas(self):
+        return self._heap
+
     def __repr__(self):
         vals = [str(i) for i in self._heap]
         return '->'.join(vals)
+
+
+def get_array_top_k():
+    """获取数组 top k值"""
+    arr = [0, 7, 3, 4, 2, 1, 5, 6]
+    arr_length = len(arr)
+    top_k = 3
+    arr_heap = arr[:top_k]  # 取数组前三个元素建堆
+    heap = MinHeap(arr_heap)
+    heap.heapify()  # 初始建堆
+    for val in arr[top_k:]:
+        # 依次扫描后面的数据
+        root_val = heap.heap_datas[0]
+        if val > root_val:
+            heap.heappop()  # 删除堆顶元素
+            heap.heappush(val)  # 插入新元素
+    logger.info(heap)
+    arr.append(10)
+    arr.append(-1)
+    arr.append(11)
+    if len(arr) > arr_length:
+        for val in arr[arr_length:]:
+            # 求取最新arr 的 top k
+            root_val = heap.heap_datas[0]
+            if val > root_val:
+                heap.heappop()  # 删除堆顶元素
+                heap.heappush(val)  # 插入新元素
+        arr_length = len(arr)  # 更新数据长度
+    logger.info(heap)
+
+
+def merge_n_sort_array():
+    """使用最小堆合并n个有序数组"""
+    n_arr = [[1, 3, 4, 5, 8],
+             [2, 4, 6, 7, 11],
+             [8, 9, 10, 12, 15]]
+
+    datas = []  # 获取初始堆的元素, 即所有数组的第一个元素
+    for arr in n_arr:
+        datas.append(arr.pop(0))
+
+    index_datas = datas.copy()  # 用于存取每次堆顶元素属于哪个数组, index_datas元素的索引对应数组索引
+    sort_big_arr = []
+    # 建立最小堆
+    min_heap = MinHeap(datas)
+    min_heap.heapify()
+
+    index, arr_num = 0, len(n_arr)
+    while True:
+        try:
+            root_val = min_heap.heappop()  # 获取堆顶元素
+            sort_big_arr.append(root_val)
+            arr_index = index_datas.index(root_val)
+            new_val = n_arr[arr_index].pop(0)  # 从堆顶元素对应的数组中取下一个元素, 数组越界跳转
+            index_datas[arr_index] = new_val
+            min_heap.heappush(new_val)  # 新元素入堆
+        except IndexError as e:
+            # 出现索引异常时, 说明数据已经访问完
+            logger.info("Error info: {}".format(e))
+            index += 1
+            index_datas[arr_index] = None  # 当前数组数据已经访问完, 对应索引数组设置为None
+            if index >= arr_num:
+                # 数组已经全部访问完
+                break
+
+    logger.info(sort_big_arr)
 
 
 if __name__ == '__main__':
@@ -116,3 +186,5 @@ if __name__ == '__main__':
     # logger.info(h)
     # logger.info(h.heappop())
     # logger.info(h)
+    # get_array_top_k()
+    merge_n_sort_array()
