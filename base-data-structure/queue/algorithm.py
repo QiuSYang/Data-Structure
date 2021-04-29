17f3acc1f5c0399c74d3411d00466d563f0fdf74
杨球松 <yangqiusong@cloudwalk.cn>
2021-04-29 20:29:52 +0800

    "add recursion tree"

---
+++ base-data-structure/queue/algorithm.py
@@ -0,0 +1,47 @@
+"""记录一些算法题"""
+
+from typing import List
+from queue import Queue
+
+
+def numIslands(grid: List[List[str]]) -> int:
+    """广度(宽度)优先搜索-bfs"""
+    if not grid:
+        return 0
+    direct_coors = [(0, -1), (-1, 0), (0, 1), (1, 0)]  # 四个方位offset
+
+    index = 1  # 岛屿ID
+    q = Queue()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "1":
+                index += 1
+                print("index value: {}".format(index))
+                grid[i][j] = index  # 已经搜索过的位置置0
+
+                q.put((i, j))  # 入列
+                while True:
+                    if q.empty():
+                        break
+                    cur = q.get()  # 元素出列
+                    for offset in direct_coors:
+                        # 四个方向广度优先搜索
+                        row, col = cur[0] + offset[0], cur[1] + offset[1]
+                        if (row < 0 or row >= len(grid)
+                                or col < 0 or col >= len(grid[0])):
+                            continue
+                        if grid[row][col] == "1":
+                            q.put((row, col))
+                            grid[row][col] = str(index)  # 近邻周围值设置为index
+
+    return index - 1
+
+
+if __name__ == '__main__':
+    arr = [["1","1","0","0","0"],
+           ["1","1","0","0","0"],
+           ["0","0","1","0","0"],
+           ["0","0","0","1","1"]]
+
+    result = numIslands(arr)
+    print(result)
